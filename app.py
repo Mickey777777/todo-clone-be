6a225c893e279ce3adb8d755e266b108eac8d727
Mickey777777 <wh879642@gmail.com>
2025-11-19 10:02:55 +0900

    feat: cors설정

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
+from flask_cors import CORS
 from routes.todos import todos_bp
-from models import db, User
+from models import db
 from routes.auth import auth_bp
 from dotenv import load_dotenv
 import os
@@ -9,6 +10,7 @@ import os
 load_dotenv()
 
 app = Flask(__name__)
+CORS(app, supports_credentials=True)  # 세션 쿠키 허용
 
 # Blueprint 등록
 app.register_blueprint(todos_bp)
